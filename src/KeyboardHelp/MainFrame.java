6731481f8d9c315846a4dbf1c0150b106d24dfd0
HPLap <alonzoftware@gmail.com>
2022-12-26 08:01:35 -0400

    HPLap 20221226 08:01 Add Tab with JPanels

--- src/KeyboardHelp/MainFrame.java
+++ src/KeyboardHelp/MainFrame.java
@@ -1,12 +1,21 @@
 package KeyboardHelp;
 import com.apple.eawt.Application;
 import javax.swing.*;
+import java.awt.*;
 
 public class MainFrame extends JFrame {
+
+    //Containers
+    JTabbedPane jTabbedPaneMain;
+    JPanel jPanelLogicChars;
+    JPanel jPanelSpecialChars;
+
 
     //MAIN WINDOW
     public MainFrame() {
         basicFrameConfiguration();
+        createContainers();
+        configContainers();
     }
     private void basicFrameConfiguration(){
         this.setTitle("Keyboard Helper - Developed By Alonzoftware");
@@ -24,5 +33,29 @@ public class MainFrame extends JFrame {
         this.setIconImage(img.getImage());
         //this.setLayout(null);
 
+    }
+    private void createContainers (){
+        //TABS
+        //===========
+        //Tab Main
+        //=============================================
+        jTabbedPaneMain = new JTabbedPane();
+        jTabbedPaneMain.setBounds(100, 100, 512, 512);
+        //PANELS
+        //===========
+        //Main Panels
+        jPanelLogicChars = new JPanel(new BorderLayout());
+        jPanelLogicChars.setLayout(new BorderLayout());
+        jPanelSpecialChars = new JPanel(new BorderLayout());
+        jPanelSpecialChars.setLayout(new BorderLayout());
+        //----------------------------------------
+    }
+    private void configContainers (){
+        //=========================================
+        this.add(jTabbedPaneMain);
+        jTabbedPaneMain.add("Servidor Local", jPanelLogicChars);
+        jTabbedPaneMain.add("Servidor Nacional", jPanelSpecialChars);
+        //=========================================
+        //this.setVisible(true);
     }
 }
