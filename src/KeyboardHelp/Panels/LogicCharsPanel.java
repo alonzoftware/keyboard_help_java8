3a6e3bc0817cd165651b047291fe4d8391ace0ee
AsusPC <alonzoftware@gmail.com>
2023-01-05 15:29:52 -0400

    AsusPC 20230105 15:28 Add Panels Package

---
+++ src/KeyboardHelp/Panels/LogicCharsPanel.java
@@ -0,0 +1,12 @@
+package KeyboardHelp.Panels;
+
+import javax.swing.*;
+
+public class LogicCharsPanel extends JPanel {
+    //1st ROW
+    //=================================================
+    JLabel lblTitSpeed;
+    public JTextField txtCiclosTestSpeed,txtTiempoInicioSpeed;
+
+
+}
